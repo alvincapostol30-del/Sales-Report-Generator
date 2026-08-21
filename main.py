b12a049eddec5cb4c325d5ae773bfd0362fd892f
Alvin Apostol <alvin.c.apostol26@gmail.com>
2026-08-21 11:57:55 +0800

    Read sales data from CSV

--- main.py
+++ main.py
@@ -2,4 +2,8 @@ import csv
 with open("Input/sales.csv", newline="") as file:
     reader = csv.DictReader(file, delimiter="\t")
     for row in reader:
-        print(row)
+        print(f"Product: {row['Product']}")
+        print(f"Salesperson: {row['Salesperson']}")
+        print(f"Order ID: {row['Quantity']}")
+        print(f"Unit Price: {row['Unit Price']}")
+        print("-" * 40)
